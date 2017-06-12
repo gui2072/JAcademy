50d42b99af9936f3f0786c5b23ef4e954361ae50
Nitai Charan <nitaicharan@gmail.com>
2017-06-12 10:21:14 -0300

    Documentação RepositorioAluno

--- src/br/unipe/cc/mpl3/jacademy/fachada/FachadaTurma.java
+++ src/br/unipe/cc/mpl3/jacademy/fachada/FachadaTurma.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Retorna conjunto de Objetos Turma para tratamento na interfece
+ * Retorna lista de Objetos Turma para tratamento na interfece
  * grafica.
  * Posibilita query's para resgate de turma (SELECT * FROM turma query) 
  * no BD.
  * 
- * @author Nitai Charan
+ * @author paulo
  * @param query
- * @return Resultado da query realizado no BD em um conjunto Set de Turma.
+ * @return Resultado da query realizado no BD em um conjunto List de Turma.
 */
 public class FachadaTurma {
     public static List getDadoTurma(String query) {
